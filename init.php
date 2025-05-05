97875fffa950be12901dd81620eeb324ce9643a0
Andrey2109 <andrey@podiumcomp.com>
2025-05-05 05:39:06 +0300

    Add init.php file and include it in header files for session management

---
+++ init.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+require_once "helpers.php";
+define('APP_NAME', 'CMS PDO SYSTEM');
--- partials/admin/header.php
+++ partials/admin/header.php
@@ -1,3 +1,7 @@
+<?php
+include __DIR__ .  '/../../init.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
