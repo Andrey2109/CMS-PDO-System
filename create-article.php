aac91178f13f4bed54bedac0a0d07907cc00f854
Andrey2109 <andrey@podiumcomp.com>
2025-05-20 06:53:44 +0300

    Enhance article creation by adding image upload functionality and updating form input type; ensure directory creation for uploads.

--- create-article.php
+++ create-article.php
@@ -1,15 +1,50 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 include "./partials/admin/header.php";
 include "./partials/admin/navbar.php";
 
 if (isPostRequest()) {
     $title = $_POST['title'];
     $content = $_POST['content'];
-    $image = $_POST['image'];
     $date = $_POST['date'];
     $user_id = $_SESSION['user_id'];
     $article = new Article();
-    if ($article->createArticle($title, $content, $image, $date, $user_id)) {
+
+
+    $targetDir = 'uploads' .  DIRECTORY_SEPARATOR;
+    $imagePath = '';
+    $error = '';
+
+
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0777, true);
+    }
+
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === 0) {
+        // var_dump($_FILES);
+        $targetFile = $targetDir . $_FILES['image']['name'];
+        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+
+        if (in_array($imageFileType, $allowedTypes)) {
+            echo 'we are here';
+            if (move_uploaded_file($_FILES['image']["tmp_name"], $targetFile)) {
+                $imagePath = $targetFile;
+                echo $imagePath;
+            } else {
+
+                $error = 'There was an error uploading the file';
+                echo $error;
+            }
+        } else {
+            $error = 'Only JPG, JPEG, PNG and GIF types are allowed';
+            echo $error;
+        }
+    }
+    if ($article->createArticle($title, $content, $imagePath, $date, $user_id)) {
         redirect('admin.php');
     }
 }
@@ -33,8 +68,8 @@ if (isPostRequest()) {
             <textarea name="content" class="form-control" id="content" rows="10" placeholder="Enter article content" required></textarea>
         </div>
         <div class="mb-3">
-            <label for="image" class="form-label">Featured Image URL</label>
-            <input name="image" type="url" class="form-control" id="image" placeholder="Enter image URL">
+            <label for="image" class="form-label">Featured Image</label>
+            <input name="image" type="file" class="form-control" id="image" placeholder="Upload image file">
         </div>
         <button type="submit" class="btn btn-success">Publish Article</button>
         <a href="admin.php" class="btn btn-secondary ms-2">Cancel</a>
