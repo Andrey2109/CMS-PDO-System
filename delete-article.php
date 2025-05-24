82a6c67c97fe0296035ab41eda8f2f06f148e28d
Andrey2109 <andrey@podiumcomp.com>
2025-05-24 09:50:03 +0300

    Implement article deletion with image removal in delete-article.php

--- delete-article.php
+++ delete-article.php
@@ -6,6 +6,10 @@ if (isPostRequest()) {
     if (isset($_POST['article_id'])) {
         $id = $_POST['article_id'];
         $article_obj = new Article();
+        $article = $article_obj->get_article_with_owner_by_id($id);
+        if (file_exists($article->image)) {
+            unlink($article->image);
+        }
         if ($article_obj->deleteArticle($id)) {
             redirect('admin.php');
         }
